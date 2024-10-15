7986c682a92fe3c9e8354fa8dd64411c1c4af987
mg3994 <126542773+mg3994@users.noreply.github.com>
2024-10-15 22:36:21 +0530

    making code smaller removing useless stuffs,
    TODO: is in pending, must fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,6 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown_selectionarea/flutter_markdown_selectionarea.dart';
-
-const markdownCunks = [
-  '''
-### Problem
-
-We’re building an LLM based tool for one of our FilledStacks clients. 
-  ''',
-  '''
-As with ChatGPT, the response from the LLM is streamed back to us.
-  ''',
-  '''
-The text comes back as it 
-  ''',
-  '''
-is being completed. 
-  ''',
-  '''
-Here’s an example of how
-  ''',
-  '''
-paragraph would be returned:
-  ''',
-  '''
-**The full paragraph**
-
-“I need every new
-  ''',
-  '''
-word being added to the text to animate i
-  ''',
-  '''
-n using a fade functionality. This an
-  ''',
-  '''
-example of this can be seen when using Gemini chat.”
-  ''',
-  '''
-**How it’s returned**
-
-“I need”
-  ''',
-  '''
-“I need every new word”
-  ''',
-  '''
-“I need every new word
-  ''',
-  '''
-being added to”
-  ''',
-  '''
-“I need every new word being
-  ''',
-  '''
-added to the text”
-  ''',
-  '''
-“I need every new word being added to the text to animate in”
-  ''',
-];
-
-const defaultMessage = 'Tap FAB to add markdown';
 
 void main() {
   runApp(const MyApp());
@@ -69,41 +8,114 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Animated Text Display',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AnimatedTextScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class AnimatedTextScreen extends StatefulWidget {
+  const AnimatedTextScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State createState() => _AnimatedTextScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _currentMarkdown = defaultMessage;
-  int _markdownIndex = 0;
+class _AnimatedTextScreenState extends State<AnimatedTextScreen> {
+  final List<String> _textList = [
+    '''
+### Problem
 
-  void _addMarkdown() {
-    setState(() {
-      if (_currentMarkdown == defaultMessage) {
-        _currentMarkdown = markdownCunks[_markdownIndex];
-      } else {
-        _currentMarkdown += markdownCunks[_markdownIndex];
-      }
+We’re building an LLM based tool for one of our FilledStacks clients. 
+  ''',
+    '''
+As with ChatGPT, the response from the LLM is streamed back to us.
+  ''',
+    '''
+The text comes back as it 
+  ''',
+    '''
+is being completed. 
+  ''',
+    '''
+Here’s an example of how
+  ''',
+    '''
+paragraph would be returned:
+  ''',
+    '''
+**The full paragraph**
 
-      _markdownIndex++;
+“I need every new
+  ''',
+    '''
+word being added to the text to animate i
+  ''',
+    '''
+n using a fade functionality. This an
+  ''',
+    '''
+example of this can be seen when using Gemini chat.”
+  ''',
+    '''
+**How it’s returned**
+
+“I need”
+  ''',
+    '''
+“I need every new word”
+  ''',
+    '''
+“I need every new word
+  ''',
+    '''
+being added to”
+  ''',
+    '''
+“I need every new word being
+  ''',
+    '''
+added to the text”
+  ''',
+    '''
+“I need every new word being added to the text to animate in”
+  ''',
+  ];
+
+  String _currentText = "";
+  int _index = 0;
+  final ScrollController _scrollController = ScrollController();
+
+  // Function to append new text from the list
+  void _addText() {
+    if (_index < _textList.length) {
+      setState(() {
+        _currentText += _textList[_index];
+        _index++;
+      });
+
+      // After the text has been added, scroll to the bottom  (only if content is larger)
+      _scrollToBottom();
+    }
+  }
+
+  final _defaultMessage = 'Tap FAB to add markdown';
+  // Function to scroll the content to the bottom
+  void _scrollToBottom() {
+    // just an improvement to scroll to bottom
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeOut,
+      );
     });
   }
 
@@ -111,19 +123,51 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Animated Text Display'),
       ),
       body: Center(
-          child: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 700),
-        child: MarkdownBody(
-          data: _currentMarkdown,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 700),
+          child: SingleChildScrollView(
+            controller: _scrollController, // Attach scroll controller
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 1000),
+              // TODO:
+              // try modifying this by using layoutBuilder parm (not to by default) to something else where layout related stuff is minimum
+
+              // by default there but you can modify it => it is must to modify
+              // layoutBuilder: (currentChild, previousChildren) {
+              //   return Stack(
+              //     alignment: Alignment.center,
+              //     children: <Widget>[
+              //       ...previousChildren,
+              //       if (currentChild != null) currentChild,
+              //     ],
+              //   );
+              // },
+
+              /// By default there but you can modify it
+              // transitionBuilder: (Widget child, Animation<double> animation) {
+              //   return FadeTransition(
+              //     opacity: animation,
+              //     child: child,
+              //   );
+              // },
+              child: (_currentText == "")
+                  ? Text(_defaultMessage)
+                  : MarkdownBody(
+                      data: _currentText, // The text we are animating
+                      key: ValueKey<String>(
+                          // Main thing is that No one is using Keys , and these Keys makes animation Smoother
+                          _currentText), // Assign unique key for animation
+                    ),
+            ),
+          ),
         ),
-      )),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addMarkdown,
-        tooltip: 'Increment',
+        onPressed: _addText,
+        tooltip: 'Add Text',
         child: const Icon(Icons.add),
       ),
     );
